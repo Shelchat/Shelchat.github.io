61db3d86f52724c6677f17c44e77884906f9a3ea
Shelchat <shelomith.chatmon@amtrustgroup.com>
2021-06-04 09:12:08 -0400

    email test

--- email-test.php
+++ email-test.php
@@ -1,3 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles-1.css" type="text/css"> 
+    <link rel="stylesheet" href="home-styles.css" type="text/css">
+    <title>Shelomith Inspires - Get Fit For Destiny Morning Routine</title>
+  </head>
+  <body>
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light g-0">
+            <a class="navbar-brand" href="#">Shelomith Inspires</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 <?php
 
 $to=$_POST['email'];
@@ -241,6 +277,7 @@ if(mail($to,$subject,$message,$headers)){
 else{
     echo"not sent";
 };
-
-
 ?>
+
+</body>
+</html>
